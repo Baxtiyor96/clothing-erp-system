cbc1c07a4f946f892a229d3a6b85b5c68b53c10c
Baxtiyor <baxtiyorpdp1996@gmail.com>
2026-06-12 14:05:18 +0500

    Spring Boot ERP deploy

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,176 @@
+package com.example.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDateTime;
+
+    private String createdUser;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDateTime;
+
+    private String lastModifiedUser;
+
+    private String userAddress;
+
+    private String userContactNumber;
+
+    private String userEmail;
+
+    private String userFname;
+
+    private String userLname;
+
+    private String userName;
+
+    private String password;
+
+    private Integer version;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserRole> userRoles = new ArrayList<>();
+
+    public User() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Date createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public String getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(String createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public Date getLastModifiedDateTime() {
+        return lastModifiedDateTime;
+    }
+
+    public void setLastModifiedDateTime(Date lastModifiedDateTime) {
+        this.lastModifiedDateTime = lastModifiedDateTime;
+    }
+
+    public String getLastModifiedUser() {
+        return lastModifiedUser;
+    }
+
+    public void setLastModifiedUser(String lastModifiedUser) {
+        this.lastModifiedUser = lastModifiedUser;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserContactNumber() {
+        return userContactNumber;
+    }
+
+    public void setUserContactNumber(String userContactNumber) {
+        this.userContactNumber = userContactNumber;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserFname() {
+        return userFname;
+    }
+
+    public void setUserFname(String userFname) {
+        this.userFname = userFname;
+    }
+
+    public String getUserLname() {
+        return userLname;
+    }
+
+    public void setUserLname(String userLname) {
+        this.userLname = userLname;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public List<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public UserRole addUserRole(UserRole userRole) {
+        this.userRoles.add(userRole);
+        userRole.setUser(this);
+        return userRole;
+    }
+
+    public UserRole removeUserRole(UserRole userRole) {
+        this.userRoles.remove(userRole);
+        userRole.setUser(null);
+        return userRole;
+    }
+}
